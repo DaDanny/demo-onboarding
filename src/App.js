a4ac5c2d0f30df91affe0d160a0e45f953a8b0a5
Danny Francken <danfrancken@gmail.com>
2022-04-06 12:47:44 -0700

    Deploy react starter

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} alt="logo"/>
+				<p>
+					HB Demo Onboarding
+				</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
